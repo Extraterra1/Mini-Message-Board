747225cc6db5272f110c690f0e624d2b3c666895
Carlos Pires <extraterra1@gmail.com>
2023-11-30 11:32:07 +0000

    livereload on changes

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const livereload = require('livereload');
+const connectLiveReload = require('connect-livereload');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
+});
 
 const app = express();
 
@@ -18,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(connectLiveReload());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
