9a17acb4c86e1b48127fc87f1cd84f65d129e880
Carlos Pires <extraterra1@gmail.com>
2023-11-30 11:15:34 +0000

    initial messages

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hey how you doing',
+    user: 'hunter2',
+    added: new Date()
+  },
+  {
+    text: 'Im good, thanks for asking!',
+    user: 'Alice123',
+    added: new Date()
+  },
+  {
+    text: 'Whats up?',
+    user: 'BobSmith',
+    added: new Date()
+  },
+  {
+    text: 'Feeling great today!',
+    user: 'EmilyRose',
+    added: new Date()
+  }
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
